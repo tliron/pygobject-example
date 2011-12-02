f8b36f53391a1775350ccdafbf3940ced9b5fa2d
Tal Liron <tal.liron@gmail.com>
2011-12-02 04:09:31 -0600

    Nicer construction of Python object

--- test.py
+++ test.py
@@ -2,9 +2,8 @@
 
 from gi.repository import Logging
 
-logger = Logging.Logger()
+logger = Logging.Logger(name='chipy')
 
-logger.set_property('name', 'chipy')
 logger.log("ChiPy thanks crowdSPRING for hosting!")
 logger.log("And can't wait for 'beer time'.")
 logger.log("The Ruby folk are probably drinking vitamin water right now.")
